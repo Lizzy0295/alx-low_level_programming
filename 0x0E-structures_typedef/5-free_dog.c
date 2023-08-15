80747ecfd0cbc5b2d5fdd869789b9babb7fa1a5a
lizzy0295 <lizzyjacinta95@gmail.com>
2023-08-14 18:24:53 -0700

    free_dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - functions that free dogs
+ * @d: dog to be freed
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
