bf58d62639447ec9df05275df22de36052b7254b
lizzy0295 <lizzyjacinta95@gmail.com>
2023-08-16 16:23:41 -0700

    Do hard things

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,30 +3,44 @@
 #include <stdlib.h>
 
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
+ * main - perform calculator functions on command line
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Return: ...
+ * Return: results of calc function execution
  */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	char *op; /* operator */
+	int num1, num2; /* number inputs */
 
-	if (argc != 4)
+	if (argc != 4) /* if argument count is wrong */
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	/* set cmd argument input */
+	num1 = atoi(argv[1]); /* first number input */
+	op = (argv[2]); /* operator input */
+	num2 = atoi(argv[3]); /* second number input */
 
-	if (!oprt)
+	/* if operator not in operator list */
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	/* if user tries to divide or perform modulus by 0 */
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	/* print results of operation */
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
