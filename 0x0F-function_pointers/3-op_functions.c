de9f84e75790f84a3eb501d44589b2242675a927
lizzy0295 <lizzyjacinta95@gmail.com>
2023-08-16 16:20:22 -0700

    Do hard things

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,13 +1,13 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * op_add - ...
- * @a: ...
- * @b: ...
+ * op_add - sum two numbers
+ * @a: first number
+ * @b: second number
  *
- * Return: ...
+ * Return: sum of the two numbers
  */
 int op_add(int a, int b)
 {
@@ -15,11 +15,11 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - ...
- * @a: ...
- * @b: ...
+ * op_sub - subtract two numbers
+ * @a: first number
+ * @b: second number
  *
- * Return: ...
+ * Return: difference of a and b
  */
 int op_sub(int a, int b)
 {
@@ -27,11 +27,11 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - ...
- * @a: ...
- * @b:...
+ * op_mul - multiply two numbers
+ * @a: first number
+ * @b: second number
  *
- * Return:...
+ * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
@@ -39,11 +39,11 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
+ * op_div - divide two numbers
+ * @a: first number
+ * @b: second number
  *
- * Return: ...
+ * Return: result of division of a by b
  */
 int op_div(int a, int b)
 {
@@ -51,16 +51,13 @@ int op_div(int a, int b)
 }
 
 /**
- * op_mod - ...
- * @a: ...
- * @b: ...
+ * op_mod - remainder of the division of two numbers
+ * @a: first number
+ * @b: second number
  *
- * Return: ...
+ * Return: remainder of the division of a by b
  */
 int op_mod(int a, int b)
 {
 	return (a % b);
 }
-
-
-
