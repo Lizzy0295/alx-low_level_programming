3bd586636883879020e19bb3c64e98732ed26caa
Lizzy0295 <lizzyjacinta95@gmail.com>
2024-01-15 14:46:57 -0800

    corrected 101-make_me_win.sh

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * mul -multiplication of two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
